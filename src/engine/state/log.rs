2e1371997027985da75720b1e776264eab170ddd
Altakie <artemlavrov@brandeis.edu>
2025-08-03 01:49:10 -0400

    Slimmed down log and implemented add event

--- src/engine/state/log.rs
+++ src/engine/state/log.rs
@@ -1,30 +1,31 @@
-use super::{status_effects::StatusEffect, PlayerIndex, State};
+use super::{PlayerIndex, State, status_effects::StatusEffect};
 // -- Logging --
 // TODO: Implement all events
 #[derive(Clone, Debug)]
-pub(crate) enum EventType {
+pub enum EventType {
     // Game Time Events
-    PhaseStart,
-    PhaseEnd,
+    // PhaseStart(Step),
+    // PhaseEnd(Step),
     // Player Events
-    Nomination,
+    Nomination(Player),
     Voting,
-    Execution,
-    AttemptedKill,
-    Death,
+    Execution(Player),
+    AttemptedKill(Player),
+    Death(Player),
     // Ability Specific Events
-    StatusApplied(StatusEffect)
+    StatusApplied(StatusEffect),
+    InfoLearned(String),
 }
 
 #[derive(Clone)]
-pub(crate) struct Event {
-    pub(crate) event_type: EventType,
-    pub(crate) source_player: Option<PlayerIndex>,
-    pub(crate) target_player: Option<PlayerIndex>,
+pub struct Event {
+    pub event_type: EventType,
+    pub source_player: Option<PlayerIndex>,
+    pub target_player: Option<PlayerIndex>,
 }
 
 impl Event {
-    pub(crate) fn new(
+    pub fn new(
         event_type: EventType,
         source_player: Option<PlayerIndex>,
         target_player: Option<PlayerIndex>,
@@ -36,7 +37,7 @@ impl Event {
         }
     }
 
-    pub(crate) fn new_game_event(event_type: EventType) -> Self {
+    pub fn new_game_event(event_type: EventType) -> Self {
         Self {
             event_type,
             source_player: None,
@@ -44,32 +45,32 @@ impl Event {
         }
     }
 
-    fn phase_start_event() -> Self {
-        Self {
-            event_type: EventType::PhaseStart,
-            source_player: None,
-            target_player: None,
-        }
-    }
-
-    fn phase_end_event() -> Self {
-        Self {
-            event_type: EventType::PhaseEnd,
-            source_player: None,
-            target_player: None,
-        }
-    }
-    pub(crate) fn get_description(&self) -> String {
+    // fn phase_start_event() -> Self {
+    //     Self {
+    //         event_type: EventType::PhaseStart,
+    //         source_player: None,
+    //         target_player: None,
+    //     }
+    // }
+    //
+    // fn phase_end_event() -> Self {
+    //     Self {
+    //         event_type: EventType::PhaseEnd,
+    //         source_player: None,
+    //         target_player: None,
+    //     }
+    // }
+    pub fn get_description(&self) -> String {
         todo!();
     }
 
-    pub(crate) fn get_reason(&self) -> Option<String> {
+    pub fn get_reason(&self) -> Option<String> {
         todo!();
     }
 }
 
 #[derive(PartialEq, Clone)]
-pub(crate) enum DayPhase {
+pub enum DayPhase {
     Setup,
     DayDiscussion,
     DayExecution,
@@ -77,30 +78,30 @@ pub(crate) enum DayPhase {
 }
 
 #[derive(Clone)]
-pub(crate) struct DayPhaseLog {
+pub struct DayPhaseLog {
     day_phase: DayPhase,
     log: Vec<Event>,
-    day_num: usize
+    day_num: usize,
 }
 
 // #[derive(Clone)]
-// pub(crate) struct Nychthemeron {
+// pub struct Nychthemeron {
 //     day_num: usize,
 //     day: DayPhaseLog,
 //     night: DayPhaseLog,
 // }
 
 #[derive(Clone)]
-pub(crate) struct Log {
+pub struct Log {
     // TODO: Make this a tree eventually
     day_phases: Vec<DayPhaseLog>,
 }
 
 impl Log {
-    pub(crate) fn new() -> Self {
+    pub fn new() -> Self {
         let setup_phase = DayPhaseLog {
             day_phase: DayPhase::Setup,
-            log: vec![Event::phase_start_event()],
+            log: vec![],
             day_num: 0,
         };
         Self {
@@ -108,27 +109,39 @@ impl Log {
         }
     }
 
-    pub(crate) fn next_phase(&mut self) {
+    // TODO: Write search macro that takes in optional params (think like a python function)
+
+    pub fn next_phase(&mut self) {
         // Check the latest day_phase
         match self.day_phases[self.day_phases.len() - 1].day_phase {
             DayPhase::Setup => {
-            // Create night one in log
-            let night_1 = DayPhaseLog {
-                day_phase: DayPhase::Night,
-                log: vec![Event::phase_start_event()],
-                day_num: 1,
-            };
+                // Create night one in log
+                let night_1 = DayPhaseLog {
+                    day_phase: DayPhase::Night,
+                    log: vec![],
+                    day_num: 1,
+                };
                 self.day_phases.push(night_1);
             }
-            ,
             DayPhase::DayDiscussion => todo!(),
             DayPhase::DayExecution => todo!(),
             DayPhase::Night => todo!(),
         }
     }
 
-    pub(crate) fn log_event(event : Event) {
-        todo!();
+    fn latest_phase(&mut self) -> &mut DayPhaseLog {
+        // WARN: This should never be empty anyway, but do fix this implementation
+        self.day_phases.last_mut().unwrap()
+    }
+
+    pub fn log_event(&mut self, event: Event) {
+        let latest_phase = self.latest_phase();
+        latest_phase.log(event);
     }
 }
 
+impl DayPhaseLog {
+    fn log(&mut self, event: Event) {
+        self.log.push(event);
+    }
+}
